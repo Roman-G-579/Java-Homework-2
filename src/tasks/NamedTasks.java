81a349777eb378a7cf4c3474e3e24b316e99b376
Roman G <80837901+YZYSZN-579@users.noreply.github.com>
2021-04-14 18:55:35 +0300

    Changed NamedTasks ordering algorithm

--- src/tasks/NamedTasks.java
+++ src/tasks/NamedTasks.java
@@ -1,69 +1,50 @@
 package tasks;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 
 public class NamedTasks extends Tasks {
     
     private String[] names;
-    private HashMap<String, Integer> stringToInteger = new HashMap<String, Integer>();
-    private HashMap<Integer, String> integerToString = new HashMap<Integer, String>();
-    
+
     public NamedTasks(String[] names) {
         super(names.length);
         this.names = names;
-        
-        // "builds" the HashMaps
-        {
-            integerToString.put(0, "zero");
-            integerToString.put(1, "one");
-            integerToString.put(2, "two");
-            integerToString.put(3, "three");
-            integerToString.put(4, "four");
-            integerToString.put(5, "five");
-            integerToString.put(6, "six");
-            integerToString.put(7, "seven");
-            integerToString.put(8, "eight");
-            integerToString.put(9, "nine");
-            
-            stringToInteger.put("zero", 0);
-            stringToInteger.put("one", 1);
-            stringToInteger.put("two", 2);
-            stringToInteger.put("three", 3);
-            stringToInteger.put("four", 4);
-            stringToInteger.put("five", 5);
-            stringToInteger.put("six", 6);
-            stringToInteger.put("seven", 7);
-            stringToInteger.put("eight", 8);
-            stringToInteger.put("nine", 9);
-        }
+
     }
     
     //checks whether task1 depends on task2
     public boolean dependsOn(String task1, String task2) {
-        if (!stringToInteger.containsKey(task1) || !stringToInteger.containsKey(task2)) {
-            return false;
+        int task1Index=-1, task2Index=-1;
+
+        //finds the indices of the corresponding tasks in the names array
+        for (int i = 0; i < names.length; i++) {
+            if(names[i].equals(task1)) {
+                task1Index=i;
+            }if(names[i].equals(task2)) {
+                task2Index=i;
+            }
         }
+        if(task1Index<0 || task2Index<0)
+            return false;
         
-        dependsOn(stringToInteger.get(task1), stringToInteger.get(task2));
+        super.dependsOn(task1Index,task2Index);
         return true;
     }
     
     //fills the names tasks array in an order following the given dependencies
     public String[] nameOrder() {
         int[] numberedOrder = new int[names.length];
-        String[] stringOrder = new String[names.length];
-        
-        //converts the names array to a numbers array
-        for (int i = 0; i < names.length; i++) {
-            numberedOrder[i] = stringToInteger.get(names[i]);
+        String[] orderedArray = new String[names.length];
+
+        numberedOrder = super.order();
+
+        if(numberedOrder==null) {
+            return null;
         }
-        
-        numberedOrder = order();
-        
-        // converts the ordered int array into an ordered string array
+        //matches the tasks name to the corresponding order provided by the order method
         for (int i = 0; i < names.length; i++) {
-            stringOrder[i] = integerToString.get(numberedOrder[i]);
+            orderedArray[i] = names[numberedOrder[i]];
         }
-        return stringOrder;
+        return orderedArray;
     }
 }
